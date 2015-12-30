73c4ee0ae37b1409644884a42d0ca77d4f2bfc4e
马壮 <maz22@ziroom.com>
2015-12-30 20:36:53 +0800

    record sql

--- src/main/webapp/resources/sql/sql.sql
+++ src/main/webapp/resources/sql/sql.sql
@@ -3,3 +3,11 @@ CREATE DATABASE `ims`CHARACTER SET utf8 ;
 CREATE TABLE `ims`.`manager`
 ( `id` FLOAT(20) NOT NULL AUTO_INCREMENT, `name` VARCHAR(50), `password` VARCHAR(50), PRIMARY KEY (`id`) ) 
 CHARSET=utf8; 
+
+CREATE TABLE `ims`.`user`
+( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50), `gender` INT, `age` INT, `level` INT, PRIMARY KEY (`id`) ) 
+CHARSET=utf8; 
+
+CREATE TABLE `ims`.`car`
+( `id` INT NOT NULL AUTO_INCREMENT, `userid` INT, `catid` INT, `load` INT, `brand` VARCHAR(50), PRIMARY KEY (`id`) ) 
+CHARSET=utf8; 
